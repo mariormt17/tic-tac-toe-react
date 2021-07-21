6e0c717b5ba800b5bb8af6598f34b39da69bfe93
Mario Mancilla <mariormt17@gmail.com>
2021-07-20 19:37:31 -0500

    Refactor the Board function to a class and extract the Game component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,119 +1,57 @@
 import React from 'react'
 import Square from './Square'
 
-export default function Board (props) {
-  const [xIsNext, setXIsNext] = React.useState(true);
-  const [squares, setSquares] = React.useState(Array(9).fill(null));
-  const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+class Board extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
 
-  return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {
-          <Square
-            value = {squares[0]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[0] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[1]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[1] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[2]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[2] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
+  handleClick (i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  renderSquare (i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render () {
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
-      <div className="board-row">
-      {
-          <Square
-            value = {squares[3]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[3] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[4]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[4] = xIsNext ? 'X' : 'O';;
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[5]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[5] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-      </div>
-      <div className="board-row">
-      {
-          <Square
-            value = {squares[6]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[6] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[7]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[7] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-        {
-          <Square
-            value = {squares[8]}
-            onClick={() => {
-              const squaresCopy = squares.slice();
-              squaresCopy[8] = xIsNext ? 'X' : 'O';
-              setSquares(squaresCopy);
-              setXIsNext(!xIsNext);
-            }}
-          />
-        }
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Board
--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './components/Board'
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+import Game from './components/Game.js'
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
   document.getElementById('root')
 );
